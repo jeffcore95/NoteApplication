66605c3f5764af08bcc60e9bae5e354354129273
jeffcore95 <37320931+jeffcore95@users.noreply.github.com>
2021-09-15 04:56:43 +0800

    update

--- app/src/main/java/com/example/noteapplication/auth/Login.java
+++ app/src/main/java/com/example/noteapplication/auth/Login.java
@@ -2,19 +2,13 @@ package com.example.noteapplication.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.noteapplication.MainActivity;
-import com.example.noteapplication.R;
-import com.example.noteapplication.Splash;
 import com.example.noteapplication.databinding.ActivityLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -53,14 +47,6 @@ public class Login extends AppCompatActivity {
             fAuth.signInWithEmailAndPassword(loginEmail,loginPassword).addOnSuccessListener(authResult -> {
                 Toast.makeText(Login.this, "Login Successful !", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
-
-                if(fAuth.getCurrentUser().isAnonymous()){
-                    FirebaseUser user = fAuth.getCurrentUser();
-
-                    db.collection("notes").document(user.getUid()).delete().addOnSuccessListener(unused -> Toast.makeText(Login.this, "All Temp Notes Deleted.", Toast.LENGTH_SHORT).show());
-                    user.delete().addOnSuccessListener(unused -> Toast.makeText(Login.this, "Temp User Deleted.", Toast.LENGTH_SHORT).show());
-                }
-
                 finish();
             }).addOnFailureListener(e -> {
                 Toast.makeText(Login.this, "Login Failed. " + e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -85,8 +71,12 @@ public class Login extends AppCompatActivity {
                 }).setNegativeButton("I don't care", (dialog, which) -> {
                     // ToDo: delete all the notes created by Anonymous user
                     // ToDo: delete the Anonymous user
+                    if(fAuth.getCurrentUser().isAnonymous()){
+                        final FirebaseUser user = fAuth.getCurrentUser();
 
-
+                        //db.collection("notes").document(user.getUid()).delete().addOnSuccessListener(unused -> Toast.makeText(Login.this, "All Temp Notes Deleted.", Toast.LENGTH_SHORT).show());
+                        user.delete().addOnSuccessListener(unused -> Toast.makeText(Login.this, "Temp User Deleted.", Toast.LENGTH_SHORT).show());
+                    }
                 });
 
         warning.show();
